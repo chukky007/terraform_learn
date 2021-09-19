3abf5e45b2e99c0e98e83ba27b303177c4159268
Chuka Oraekwuotu <56433243+chukky007@users.noreply.github.com>
2021-09-19 22:47:05 +0100

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "access_key" {
-  type       = string
-  default = "AKIAV4QXQAX2KWIG4MKO"
-}
-
-variable "secret_key" {
-  type       = string
-  default = "ZEBhz3pjOBu1MJlwDorAmh13t7NlkppL9e5vOM8s"
-}
